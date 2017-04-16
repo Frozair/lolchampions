d0fce2e271cc5ae928ac2beb539216cd51e359fb
Rudolph Newball <newrdev@gmail.com>
2017-04-15 22:26:32 -0500

    Update css for main content

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 import ChampionsList from './containers/ChampionsList'
+
+const Content = styled.section`
+  padding: 100px 10% 150px 15%;
+`
 
 const Routes = (props) => (
   <Router {...props}>
     <div className="page">
       <div className="header"></div>
 
-      <div className="content">
+      <Content className="content">
         <Route exact path="/" component={ChampionsList} />
         {/* <Route path="champion/:id" component={Champion}/> */}
         {/* <Route path="*" component={InvalidPage}/> */}
-      </div>
+      </Content>
 
       <div className="footer"></div>
     </div>
