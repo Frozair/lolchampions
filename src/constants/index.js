5f66a145fd4b7cd9d7c0d568d86043955bce70ed
Rudolph Newball <newrdev@gmail.com>
2017-04-15 22:26:43 -0500

    Constant url for images

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,6 @@ export const INITIAL_STATE = fromJS({
   loading: false,
   fetching: false
 })
+
+export const SQUARE_IMAGE_URL = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion'
+export const SPLASH_IMAGE_URL = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash'
