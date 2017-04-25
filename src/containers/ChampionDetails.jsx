223b76f5cbc542022414f03bd28371c0a12b3c15
Rudolph Newball <newrdev@gmail.com>
2017-04-25 01:32:57 -0500

    Champion details page includes lore with a typewriter animation. TODO - Fix bug where the lore is saved from the previous champion

--- src/containers/ChampionDetails.jsx
+++ src/containers/ChampionDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { connect } from 'react-redux'
+import Typist from 'react-typist'
+import renderHTML from 'react-render-html'
 
 import { getChampion } from '../actions/actions'
 import { SPLASH_IMAGE_URL } from '../constants'
@@ -12,7 +14,31 @@ const Wrapper = styled.section`
   color: white;
 `
 
-const loadData = ({getChampion, match}) => {  
+const DetailsWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`
+
+const typing = keyframes`
+  from{ width: 0 }
+  to{ width: 100% }
+`
+
+const LoreOverlay = styled.div`
+  width: 30%;
+  background-color: black;
+  color: papayawhip;
+  height: 100%;
+  position: absolute;
+  opacity: 0.7;
+  display: inline-block;
+  margin-left: 0px;
+  padding: 5px 10px 0 5px;
+  overflow-y: scroll;
+  word-wrap: break-word;
+`
+
+const loadData = ({getChampion, match}) => {
   getChampion(match.params.id)
 }
 
@@ -30,15 +56,27 @@ class ChampionDetails extends React.Component {
     return SPLASH_IMAGE_URL + '/' + imageName + '_' + skins.get(randomNumber).get('num') + '.jpg'
   }
 
+  getLore() {
+    return this.props.champion.get('lore')
+  }
+
   buildChampion() {
     if (this.props.champion === undefined) {
       return ''
     }
 
+console.log(this.getLore())
     return (
       <div>
         <h1>{this.props.champion.get('name')}</h1>
-        <img src={this.getRandomSkin()} alt="Champion Skin" />
+        <DetailsWrapper>
+          <LoreOverlay>
+            <Typist avgTypingDelay={40}>
+              {renderHTML(this.getLore())}
+            </Typist>
+          </LoreOverlay>
+          <img src={this.getRandomSkin()} alt="Champion Skin" />
+        </DetailsWrapper>
       </div>
     )
   }
